7bed375888e6b1c12e03eeac0c4ca654f18ad340
Tom Johnson <TJohnson@ITs-MacBook-Pro-3.local>
2016-05-31 13:40:36 -0700

    action and reducer plus tests

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
+class CommentBox extends Component {
   // This is a controlled component, meaning it's value is determined by state, not be what user is typing in
   constructor(props) {
     super(props);
@@ -14,18 +16,27 @@ export default class CommentBox extends Component {
   
   handleSubmit(event) {
     event.preventDefault();
+    
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' });
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
-        <textarea
-          value={this.state.comment}
-          onChange={this.handleChange.bind(this)}
-        />
-        <button action="submit">Submit Comment</button>
+        <h4>Add a comment</h4>
+        <div>
+          <textarea
+            value={this.state.comment}
+            onChange={this.handleChange.bind(this)}
+          />
+          <div>
+            <button action="submit">Submit Comment</button>
+          </div>
+        </div>
       </form>
     )
   }
 }
+
+export default connect(null, actions)(CommentBox);
